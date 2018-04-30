c773ce39e5aedb4771bcbaacd86af90db4937f12
quadrrem <remo.quadrelli@gmail.com>
2018-04-30 22:14:11 +0200

    exercises -> 16

---
+++ ex16/ex16.rb
@@ -0,0 +1,30 @@
+filename = ARGV.first
+
+puts "We're going to erase #{filename}"
+puts "If you don't want that, hit CTRL-C (^C)."
+puts "If you do want that, hit RETURN."
+
+$stdin.gets
+
+puts "Opening the file ..."
+target = open(filename, 'w')
+
+puts "Truncating the file. Goodbye!"
+target.truncate(0)
+
+puts "Now I'm going to ask you for three lines."
+
+print "line 1: "
+line1 = $stdin.gets.chomp
+print "line 2: "
+line2 = $stdin.gets.chomp
+print "line 3: "
+line3 = $stdin.gets.chomp
+
+puts "I'm going to write these to the file."
+
+formatter = "%s\n%s\n%s\n"
+target.write(formatter % [line1, line2, line3])
+
+puts "And finally, we close it."
+target.close
