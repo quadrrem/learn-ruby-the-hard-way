ea77a29af053c52a61f0f84cdeb6243194e29865
quadrrem <remo.quadrelli@gmail.com>
2018-04-30 20:41:06 +0200

    exercise 1 - 8

---
+++ exp5.rb
@@ -0,0 +1,16 @@
+name = 'Remo Quadrelli'
+age = 32
+height = 190 # cm
+weight = 96 # kg
+eyes = 'Blue'
+teeth = 'Withe'
+hair = 'Brown'
+
+puts "Let's talt about #{name}"
+puts "He's #{height}."
+puts "He's #{weight}."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
